e7f480dc2a41571e9967f4a3b936f7917772127b
Łukasz Socha <l.socha@sopamo.de>
2023-03-06 10:29:12 +0100

    fix the code

--- package/src/Drivers/Shmop/ShmopDriver.php
+++ package/src/Drivers/Shmop/ShmopDriver.php
@@ -6,13 +6,21 @@ use Exception;
 use Shmop;
 use Sopamo\ClusterCache\Drivers\MemoryDriverInterface;
 use Sopamo\ClusterCache\Exceptions\MemoryBlockDoesntExistException;
+use Sopamo\ClusterCache\Exceptions\NotExistedFunctionException;
 
 class ShmopDriver implements MemoryDriverInterface
 {
     const METADATA_LENGTH_IN_BYTES = 8;
 
+    /**
+     * @throws NotExistedFunctionException
+     */
     public function put(string $memoryKey, mixed $value, int $length): bool
     {
+        if (!function_exists('shmop_write')) {
+            throw new NotExistedFunctionException('shmop_write doesnt exist');
+        }
+
         try {
             $shmop = $this->openOrCreateMemoryBlock($memoryKey, $length + self::METADATA_LENGTH_IN_BYTES,
                 ShmopConnectionMode::Create);
@@ -27,10 +35,14 @@ class ShmopDriver implements MemoryDriverInterface
     }
 
     /**
-     * @throws MemoryBlockDoesntExistException
+     * @throws MemoryBlockDoesntExistException|NotExistedFunctionException
      */
     private function openOrCreateMemoryBlock(int $memoryKey, int $length, ShmopConnectionMode $mode): Shmop
     {
+        if (!function_exists('shmop_open')) {
+            throw new NotExistedFunctionException('shmop_open doesnt exist');
+        }
+
         $shmop = shmop_open($memoryKey, $mode->value, 0644, $length + self::METADATA_LENGTH_IN_BYTES);
         if (!$shmop) {
             throw new MemoryBlockDoesntExistException('the memory block "'.$memoryKey.'" doesn\'t exist');
@@ -38,8 +50,15 @@ class ShmopDriver implements MemoryDriverInterface
         return $shmop;
     }
 
+    /**
+     * @throws NotExistedFunctionException
+     */
     public function get(string $memoryKey, int $length): mixed
     {
+        if (!function_exists('shmop_read')) {
+            throw new NotExistedFunctionException('shmop_read doesnt exist');
+        }
+
         try {
             $shmop = $this->openOrCreateMemoryBlock($memoryKey, $length + self::METADATA_LENGTH_IN_BYTES,
                 ShmopConnectionMode::ReadOnly);
@@ -50,8 +69,15 @@ class ShmopDriver implements MemoryDriverInterface
         }
     }
 
+    /**
+     * @throws NotExistedFunctionException
+     */
     public function delete(string $memoryKey, int $length): bool
     {
+        if (!function_exists('shmop_delete')) {
+            throw new NotExistedFunctionException('shmop_delete doesnt exist');
+        }
+
         try {
             $shmop = $this->openOrCreateMemoryBlock($memoryKey, $length + self::METADATA_LENGTH_IN_BYTES,
                 ShmopConnectionMode::ReadAndWite);
---
+++ package/src/Exceptions/NotExistedFunctionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sopamo\ClusterCache\Exceptions;
+
+use Exception;
+
+class NotExistedFunctionException extends Exception
+{
+
+}
